1dddb89347a8ee7629f82dc4ff4050d41e2327b9
SeriousMak <sashamkr@mail.ru>
2024-11-17 00:18:43 +0300

    rewrite echo comment

--- test.sh
+++ test.sh
@@ -9,3 +9,12 @@ echo $file_name
 read -p "Введите нужное расширение:" new_ext
 
 echo $new_ext
+
+# Извлечение имени файла без расширения и новое имя
+new_file_name="${file_name%.*}.${new_ext}"
+
+# Добавление файла new_file_name с содержимым File_name
+
+cp "$file_name" "$new_file_name"
+
+echo "Создан файл с новым крутым расширением $new_file_name, также сохранился старый $file_name"
